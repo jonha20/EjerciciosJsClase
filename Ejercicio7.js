aed30b5b739bac485e1b7945cc1ba37f35c1d915
jonha20 <jonathan812000@gmail.com>
2025-03-13 19:50:58 +0100

    ejercicio 7

--- Ejercicio7.js
+++ Ejercicio7.js
@@ -1,7 +1,22 @@
 /*Ejercicio 3
 Escriba un programa que pida un año y que escriba si es bisiesto o no. Se recuerda que los años bisiestos son múltiplos de 4, pero los múltiplos de 100 no lo son, aunque los múltiplos de 400 sí.
 */
-let fecha = Date(prompt("Introduce fecha: "))
+function añoBisiesto(fecha){
+    const splitaño = fecha.split(" ");
+    const año = splitaño[3]
+    let resultado = ""
+    if (parseInt(año) % 400 == 0) {
+        resultado = "El año " + año + " es bisiesto"
+    }else if (parseInt(año) % 100 == 0) {
+        resultado = "El año " + año + " no es bisiesto"
+    }else if (parseInt(año) % 4 == 0) {
+        resultado = "El año " + año + " es bisiesto"
+    }else{
+        resultado = "El año " + año + " no es bisiesto"
+    }
+    return resultado;
+}
 
-fecha = fecha.getFullYear();
-console.log(fecha);
+const date = new Date(prompt("Introduce fecha en formato americano: ")).toDateString('es-ES')
+console.log(añoBisiesto(date));
+
