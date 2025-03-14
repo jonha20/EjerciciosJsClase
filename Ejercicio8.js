ce8b175060d50de063aecdbfd70e07a16c20cc1e
jonha20 <jonathan812000@gmail.com>
2025-03-14 14:03:27 +0100

    Ejercicio 8 resuelto

--- Ejercicio8.js
+++ Ejercicio8.js
@@ -3,6 +3,20 @@ Contar todas las "o" almacenadas en un array de palabras. En cada posición del 
 Ejemplo de datos y llamada a la función
 let data = ["yoda", "best", "has", "tortilla", "cAfe", "barrita tomate", "zumo"];
 contarCaracteres(data,"a");*/
+
+function contarLetras(arrayPalabras) {
+  let num = 0;
+  data.forEach((palabras) => {
+    const separacion = palabras.split("");
+    separacion.forEach((letras) => {
+      if (letras == "a") {
+        num++;
+      }
+    });
+  });
+  return num;
+}
+
 const data = [
   "yoda",
   "best",
@@ -12,33 +26,4 @@ const data = [
   "barrita tomate",
   "zumo",
 ];
-const letra = []
-letra = data.split("")
-
-let num = 0;
-for (let i = 0; i < data.length; i++) {
-    data[i].split("")
-    /*for (let x = 0; x < letra.length; x++) {
-        if (String(letra[x]) == "o") {
-            num++
-        }
-    }*/
-}
-
-console.log(letra);
-
-
-/*data.forEach(palabras => {
-    palabras.forEach(letras =>{
-        
-    })
-})*/
-/*data.forEach((palabras) => {
-  palabras.split("");
-  palabras.forEach((letra) => {
-    if (letra == "o") {
-      num++;
-    }
-  });
-});
-*/
+console.log("Hay " + contarLetras(data) + " A en el texto");
